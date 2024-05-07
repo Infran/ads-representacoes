1ba9ccff4109f53fabc28316e14d8151401b606d
Infran <64187114+Infran@users.noreply.github.com>
2024-05-07 19:54:05 -0400

    login start

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,7 @@ import { Home } from "./pages/Home/Home";
 import { CreateProduct } from "./pages/CreateProduct/CreateProduct";
 import { CreateClient } from "./pages/CreateClient/CreateClient";
 import DefaultLayout from "./layouts/DefaultLayout";
+import { Login } from "./components/Login/Login";
 
 const AppRouter = () => {
   return (
@@ -14,6 +15,7 @@ const AppRouter = () => {
             <Route path="/Home" element={<Home />} />
             <Route path="/CriarProduto" element={<CreateProduct />} />
             <Route path="/CriarCliente" element={<CreateClient />} />
+            <Route path="/Login" element={<Login />} />
           </Route>
         </Routes>
       </Router>
