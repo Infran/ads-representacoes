065c4127e13faad6b2bdb4ce3c232113bdfed482
HgGabriel <hiagogabrieloliveirapinto@gmail.com>
2025-02-25 23:54:03 -0300

    add measurementId to Firebase configuration

--- src/firebase.ts
+++ src/firebase.ts
@@ -16,13 +16,13 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
 export const db = getFirestore(app);
 
 
